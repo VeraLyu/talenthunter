d0b2294f3cf371adf3aa4febe249622d0510eda5
Royce Lv <lvroyce0210@gmail.com>
2016-07-04 06:17:31 +0800

    Move all components to folder
    In order to add redux, move all current components to the "components"
    folder.

--- js/main.js
+++
@@ -1,5 +0,0 @@
-import {render} from 'react-dom';
-import React from 'react';
-import {App} from "./app.js";
-
-render(<App/>, document.getElementById('talent'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    entry: "./js/main.js",
+    entry: "./js/components/main.js",
     output: {
         publicPath: "/dist/",
         path: __dirname + "/dist",

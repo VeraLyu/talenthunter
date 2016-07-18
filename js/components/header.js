5caf7ce2f4352f262d3bd5dd5647ca80a8bb1327
Royce Lv <lvroyce0210@gmail.com>
2016-07-18 15:59:08 +0800

    Add more css

--- js/components/header.js
+++ js/components/header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-import styles from '../../scss/header.scss';
+import headStyle from '../../scss/header.scss';
+import publicStyle from '../../scss/utils.scss';
+
 
 const Header = () => (
-    <header className={styles.header}>
-      <img src="./assets/snipper.png"/>
-      <h3>Big data based talents hunting</h3>
+    <header className={`${headStyle.header} ${publicStyle.clear}`}>
+        <img src="./assets/snipper.png"/>
+        <h3>Big data based talents hunting</h3>
     </header>
 );
 

cc6bd5ef96b064a796a63e04cd30869b795f8abe
Royce Lv <lvroyce0210@gmail.com>
2016-07-25 13:52:52 +0800

    Use real prop instead of fake one
    Substitute fake props with real server side got props

--- js/components/searchresult.js
+++ js/components/searchresult.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Col} from 'react-bootstrap';
-import FaEnvelope from 'react-icons/lib/fa/envelope';
+//import FaEnvelope from 'react-icons/lib/fa/envelope';
 import FaGroup from 'react-icons/lib/fa/group';
 import FaPieChart from 'react-icons/lib/fa/pie-chart';
 import FaMale from 'react-icons/lib/fa/male';
@@ -9,6 +9,7 @@ import FaFolderOpenO from 'react-icons/lib/fa/folder-open-o';
 
 import Style from '../../scss/namecard.scss';
 
+/*
 let myProp = {
   avatar_url: 'https://avatars.githubusercontent.com/u/6775919?v=3',
   name: 'Berkeley Mart',
@@ -18,66 +19,68 @@ let myProp = {
   followers: 249,
   email: 'my_email@gmail.com',
   html_url: 'https://github.com/BerkeleyTrue',
-  public_repos: 20
+  public_repos: 20,
+  id: "123"
 };
-
+*/
 
 const ResultItem = (props) => {
   let myClass = Style.nameCard;
-  if (myProp.hireable) {
+  let email = null;
+  if (props.hireable) {
     myClass = `${myClass} ${Style.hireable}`;
+  }
+  if (props.email) {
+    email =
+      (<div className={Style.contact}>
+        <span>{props.email}</span>
+      </div>);
   }
   return (
     <Col xs={5} className={myClass}>
+      <input className={Style.check} type="checkbox" value={props.id}/>
       <div className={Style.aside}>
-        <img src={myProp.avatar_url}/>
-        <a href={myProp.html_url}>{myProp.name}</a>
-        <p><FaGroup size={16} color="#A2A2A2"/>{myProp.company}</p>
+        <img src={props.avatar_url}/>
+        <a href={props.html_url}>{props.name}</a>
+        <p><FaGroup size={16} color="#A2A2A2"/>{props.company}</p>
       </div>
       <div className={Style.information}>
         <ul >
           <li>
-            <p>{myProp.contributions}</p>
+            <p>{props.contributions}</p>
             <FaPieChart size={30} color="#A2A2A2"/>
             <p className={Style.tip}>Contributions</p>
           </li>
           <li>
-            <p>{myProp.followers}</p>
+            <p>{props.followers}</p>
             <FaMale size={30} color="#A2A2A2"/>
             <p className={Style.tip}>Followers</p>
           </li>
           <li>
-            <p>{myProp.public_repos}</p>
+            <p>{props.public_repos}</p>
             <FaFolderOpenO size={30} color="#A2A2A2"/>
             <p className={Style.tip}>Repos</p>
           </li>
         </ul>
         <div className={Style.clearElement}/>
-        <div className={Style.contact}>
-          <FaEnvelope size={16} color="#A2A2A2"/>
-          <span>{myProp.email}</span>
-        </div>
-        <div>{myProp.hireable}</div>
+        {email}
+        <div>{props.hireable}</div>
       </div>
     </Col>
     );
 };
-/*
-  <Col xs={4} className={Style.nameCard}>
-    <img src={props.avatar_url}/>
-    <p>{props.name}</p>
-    <p>{props.company}</p>
-    <p>{props.hireable}</p>
-    <p>{props.contributions}</p>
-    <p>{props.email}</p>
-    <p>{props.followers}</p>
-    <a href={props.html_url}>{props.id}</a>
-  </Col>
-*/
+
 ResultItem.propTypes = {
   avatar_url: React.PropTypes.string,
   html_url: React.PropTypes.string,
-  id: React.PropTypes.number
+  id: React.PropTypes.number,
+  hireable: React.PropTypes.bool,
+  email: React.PropTypes.string,
+  name: React.PropTypes.string,
+  company: React.PropTypes.string,
+  contributions: React.PropTypes.number,
+  followers: React.PropTypes.number,
+  public_repos: React.PropTypes.number
 };
 
 export default ResultItem;
--- js/redux/actions/git.js
+++ js/redux/actions/git.js
@@ -83,13 +83,15 @@ export function fetchRepoPeople(repo) {
     fetch(url)
       .then(result=>result.json())
       .then((talentList)=>{
-        let tmpTalentList = talentList.slice(0, 2);
+        let tmpTalentList = talentList.slice(0, 5);
         tmpTalentList.forEach((item)=>{
           let userUrl = formatUsrURL(item.login);
           fetch(userUrl).then(result=>result.json()).then((userinfo)=>{
-            userinfo.contribution = item.contributions;
-            dispatch(updatePeople(userinfo.id, userinfo));
-            dispatch(updateRepoPeopleMap(repo.id, userinfo.id));
+            userinfo.contributions = item.contributions;
+            if (userinfo.email) {
+              dispatch(updatePeople(userinfo.id, userinfo));
+              dispatch(updateRepoPeopleMap(repo.id, userinfo.id));
+            }
           });
         });
       });
@@ -110,7 +112,7 @@ export function fetchGitPeople(repos) {
             dispatch(fetchRepoPeople(item));
           });
 */
-          let tmpItem = json.items.slice(0, 2);
+          let tmpItem = json.items.slice(0, 5);
           tmpItem.forEach((item)=>{
             dispatch(recieveGitRepo(item, key));
             dispatch(fetchRepoPeople(item));
